437d69c41e18773a57acfcdf839c3ad38f1a9677
Carl Philipp Klemm <philipp@uvos.xyz>
2023-05-16 17:13:30 +0200

    add legalese

--- eisdrt/eigendrt.h
+++ eisdrt/eigendrt.h
@@ -1,3 +1,22 @@
+/* * libeisdrt - A library to calculate EIS Drts
+ * Copyright (C) 2023 Carl Klemm <carl@uvos.xyz>
+ *
+ * This file is part of libeisdrt.
+ *
+ * libeisdrt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libeisdrt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libeisdrt.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 #include <Eigen/Core>
 
